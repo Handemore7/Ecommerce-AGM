a7a4395f0d21e867e57253bb4cfd967ce0fc7109
Handemore <handemore7@gmail.com>
2022-10-12 15:36:44 -0500

    Setup of different components and pages and tailwind installation

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+//Componente de las tarjetas de los productos, debería recibir la info y retornar el componente renderizado
+import React from 'react';
+
+
+const ProductCard = ({info}) => {
+  return (
+    <div>
+      a
+        {info._id}
+        
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Banner } from './Banner';
+export { default as CategoriesCard } from './CategoriesCard';
+export { default as NavBar } from './NavBar';
+export { default as ProductCard } from './ProductCard';
